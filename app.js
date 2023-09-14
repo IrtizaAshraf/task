29cd920798994c1a25e57f694ff125f59d7f7e90
Irtiza Ashraf <irtizaashraf802@gmail.com>
2023-09-15 01:08:08 +0500

    doine

--- app.js
+++ app.js
@@ -1,147 +1,132 @@
-const uesrs =document.querySelector('#task');
+// const uesrs =document.querySelector('#task');
+// const edit =document.querySelector('div');
+
+
+// const arry =[]
+// function renders(e) {
+//     email=" ";
+//     password=" ";
+//     // edit.innerHTML='';
+//     const obt={
+//         email, password 
+//   }
+//   arry.push(obt)
+//   console.log(arry);
+//   email=" ";
+//   password=" ";
+
+//   for (let d = 0; d < arry.length; d++) {
+//         const element = arry[e];
+//         console.log(element);
+//         edit.innerHTML +=`<p>Email: ${arry[i].email}</p>
+//         <p>password: ${arry[i].password}</p>
+//         <button onclick="deletvalue(${d})">Delete</button>
+//         <button onclick="editvalue(${d})">Edite</button>`
+
+//     }
+// }
+
+// function showwel(e) {
+//       const email =document.querySelector('#email').value;
+//       const password =document.querySelector('#password').value;
+//       const edit =document.querySelector('div');
+//       e.preventDefault()
+
+//       console.log(email);
+//       console.log(password);
+//     //   email=" ";
+//     //   password=" ";
+//       const obt={
+//             email, password 
+//       }
+//       arry.push(obt)
+//       console.log(arry);
+      
+
+//       for (let d = 0; d < arry.length; d++) {
+//             const element = arry[e];
+//             console.log(element);
+//             edit.innerHTML +=`<p>Email: ${arry[i].email}</p>
+//             <p>password: ${arry[i].password}</p>
+//             <button onclick="deletvalue(${d})">Delete</button>
+//             <button onclick="editvalue(${d})">Edite</button>`
+
+//         }
+//       renders() 
+// }
+
+// function deletvalue(index) {
+//       arry.splice(index, 1)
+//       email.value = '';
+//       names.value = '';
+//       edit.innerHTML = '';
+
+//       renders()
+
+// }
+// // console.log(uesrs.value);\
 
 
 
-const arry =[]
-function renders() {
-      email=" ";
-      password=" ";
-       
-      for (let d = 0; d < arry.length; d++) {
-            const element = arry[e];
-            console.log(element);
-            edit.innerHTML +=`<p>${element}</p>
-            <button onclick="deletvalue(${d})">Delete</button>
-            <button onclick="editvalue(${d})">Edite</button>`
+const email = document.getElementById("email");
+const password = document.getElementById("password");
+const result = document.getElementById("result")
 
-      }
+
+
+const arry = [];
+
+function render() {
+    result.innerHTML = ''
+    const user = {
+        Email: email.value,
+        Password: password.value
+    }
+    arry.push(user)
+    email.value = ""
+    password.value = ""
+    for (let i = 0; i < arry.length; i++) {
+        // console.log(arry[i]);
+        result.innerHTML += `<p>Email: ${arry[i].Email}</p>
+        <p>Password:${arry[i].Password}</p>
+        <button onclick="editvalue(${i})">Edit</button>
+        <button onclick="deletevalue(${i})">Delete</button>
+        `
+
+    }
 }
 function showwel(e) {
-      const email =document.querySelector('#email').value;
-      const password =document.querySelector('#password').value;
-      const edit =document.querySelector('div').value;
-      e.preventDefault()
-      
-      console.log(email);
-      console.log(password);
-      const obt={
-            email, password 
-      }
-      arry.push(obt)
-      console.log(arry);
-      email=" ";
-      password=" ";
-      
-      for (let d = 0; d < arry.length; d++) {
-            const element = arry[e];
-            console.log(element);
-            edit.innerHTML +=`<p>${element}</p>
-            <button onclick="deletvalue(${d})">Delete</button>
-            <button onclick="editvalue(${d})">Edite</button>`
-
-      }
-      // renders() 
-}
-
-function deletvalue(index) {
-      arry.splice(index, 1)
-      email.value = '';
-      names.value = '';
-      edit.innerHTML = '';
-      
-      renders()
-  
-}
-// console.log(uesrs.value);\
-
-
-
-
-const addlist = document.querySelector("#email");
-const addlists = document.querySelector("#password");
-const arrry = document.querySelector("div");
-const erroermassage = document.querySelector("#error-message")
-const todo = [];
-function todolist(e) {
-    e.preventDefault();
-    switch (addlist.value) {
-        case '':
-            alert("Enter the Value ")
-            break;
-        default:
-            todo.push(addlist.value)
-            addlist.value = '';
-            arrry.innerHTML = '';
-            for (let g = 0; g < todo.length; g++) {
-                const item = todo[g];
-                arrry.innerHTML += `<li>${item}</li>  
-             <button onclick="deletvalue(${g})">Delete</button>
-             <button onclick="editvalue(${g})">Edite</button>`
-                    ;
-            }
-            break;
+    if (email.value === "") {
+        
+        alert('Enter Email Password')
+    } else {
+        e.preventDefault()
+        render()
     }
-}
-function deletvalue(index) {
-    todo.splice(index, 1)
-    addlist.value = '';
-    arrry.innerHTML = '';
-    for (let g = 0; g < todo.length; g++) {
-        const item = todo[g];
-        arrry.innerHTML += `<li>${item}</li>  
-             <button onclick="deletvalue(${g})">Delete</button>
-             <button onclick="editvalue(${g})">Edite</button>`
-            ;
-    }
-    // for (let g = 0; g < todo.length; g++) {
-    //     const item = todo[g];
-    //     arrry.innerHTML += `<li>${item}</li>  
-    //          <button onclick="deletvalue(${g})">Delete</button>
-    //          <button onclick="editvalue(${g})">Edite</button>`
-    //         ;
-    // }
-    renders()
 
 }
+
+
 
 
 function editvalue(index) {
-    const todolist = +prompt("Change the Value", todo[index]) 
-    todo.splice(index, 1, todolist)
-
-
-    const content = prompt('Edit task:', todo[index]);
-    // if (content === "") {
-    //     todo[index] = content ;
-    //     renders();
-    //     alert("please Enter the value")
-    // }
-    // else{
-    //     todo[index] = content ;
-    //     renders();
-    // }
-    switch (content) {
-        case '':
-            // todo.splice(index,1, content)
-            renders()
-            alert("bhai jan  value daydoo")
-            break;
-
-        default:
-            todo.splice(index, 1, content)
-            renders()
-            break;
-    }
+    // console.log(index)
+    result.innerHTML = ''
+    const changeEmail = prompt("Change Emial", arry[index].Email)
+    const changePassword = prompt("Change Password", arry[index].Password)
+    arry[index].Email = changeEmail;
+    arry[index].Password = changePassword;
+    render()
 
 }
 
-function renders() {
-    arrry.innerHTML='';
-    for (let g = 0; g < todo.length; g++) {
-        const item = todo[g];
-        arrry.innerHTML += `<li>${item}</li>  
-             <button onclick="deletvalue(${g})">Delete</button>
-             <button onclick="editvalue(${g})">Edite</button>`
-            ;
-    }   
+function deletevalue(index) {
+    email.value='';
+    password.value='';
+    result.innerHTML = '';
+    arry.splice(index, 1)
+    render()
+    
+
+
 }
